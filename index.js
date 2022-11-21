0e0623fb62730727b05b91482149b468c653c0f3
kimathinjoki <dfkimathinjoki@gmail.com>
2022-11-21 12:35:56 +0300

    class work

--- index.js
+++ index.js
@@ -5,40 +5,59 @@ function printValue(value){
 
 // 1.0 DEFINE VARIABLES
 //TODO: ADD fixed variable
-
+const height = 5.9
 
 //TODO: ADD redeclarable variable
+var score = 100
+var scorre = 59
+score = 79
 
+printValue(score)
 
 //TODO: ADD re-assignable but not redeclarable variable
+let age = 27
+printValue(age) //27
+//var age  = 67 // you can't redeclare
 
 
 // 2.0 DIFFERENT DATA TYPES
 //TODO: Create a fixed variable with a number value
+const miles = 346
 
 
 //TODO: Create a redeclarable variable with a boolean value
 
+var sunny = true
 
 //TODO: Create a re-assignable variable with a string value
 
-
+let car = "Honda"
 
 // 3.0 STRING OPERATIONS
 //TODO: Concatenate string (+)
+let name = "Codetari "
+let lang =  "Java, python, ruby and javascript"
 
+printValue(" " + name + lang)
 
 //TODO: Interpolate string (`${}`)
 
+let prof = `"${name} drives a ${car} and he codes in ${lang}"`
+printValue(prof)
 
 //TODO: Convert to uppercase
-
+let caps = prof.toUpperCase()
+printValue(caps)
 
 //TODO: Convert to lowercase
-
-
+let smll ="THE WEATHER TODAY IS COOL"
+printValue(smll.toLowerCase())
 //TODO: Index a specific character
+printValue(name[3])
 
+    // you can use a value charAt()
+
+printValue("ABCDEFGHIJKLMNOPQRSTUVWXYZ".charAt(10));
 
 //TODO: PRACTICE ON YOUR OWN (Check mozilla documentation)
 
